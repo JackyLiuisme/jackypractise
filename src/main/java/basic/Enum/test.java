1a0532390cd3073a5621bfdbc71c741c6a49c506
lb239669 <lb239669@alibaba-inc.com>
2020-07-12 20:05:03 +0800

    latest

--- src/main/java/basic/Enum/test.java
+++ src/main/java/basic/Enum/test.java
@@ -1,10 +1,27 @@
 package basic.Enum;
 
 public class test {
+    public void judge(TestEnum testEnum){
+        switch (testEnum) {
+            case MAN:
+                System.out.println("I am man");
+                break;
+            case WOMAN:
+                System.out.println("I am woman");
+                break;
+        }
+    }
     public static void main(String[] args) {
         TestEnum man;
-        man = TestEnum.valueOf("MAN");
+       man = TestEnum.valueOf("lllMAN");
         man.setName("bitch");
+        TestEnum.MAN.setName("bbbb");
+        System.out.println(man.getName());
         System.out.println(man.toString());
+        for (TestEnum t : TestEnum.values()
+             ) {
+            System.out.println(t);
+        }
+        new test().judge(TestEnum.WOMAN);
     }
 }
